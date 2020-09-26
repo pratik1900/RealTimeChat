2d37b57e33cb1463f1af93ccd07314a73f2f1f60
Pratik <bose.pratik1900@gmail.com>
2020-09-26 15:08:39 +0530

    sidebar working

--- client/chat-client/src/components/SideBar/SideBar.js
+++ client/chat-client/src/components/SideBar/SideBar.js
@@ -4,31 +4,41 @@ import { FaBars } from "react-icons/fa";
 import Backdrop from "../Backdrop/Backdrop";
 
 export default class SideBar extends Component {
-  attachedClasses = [classes.SideBar, classes.Close];
-
-  componentDidMount() {
-    if (this.props.showSidebar === true) {
-      console.log("ComponentDidMount: SIDEBAR:", this.props.showSidebar);
-      this.attachedClasses = [classes.SideBar, classes.Open];
-    }
-  }
+  state = {
+    attachedClasses: [classes.SideBar, classes.Close],
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    // if (prevProps.showSidebar !== this.props.showSidebar) { 
-      if(this.props.showSidebar === true){
+    if (prevProps.showSidebar !== this.props.showSidebar) {
       console.log("ComponentDidUpdate: SIDEBAR:", this.props.showSidebar);
-        this.attachedClasses = [classes.SideBar, classes.Open];
+      if (this.props.showSidebar === true) {
+        this.setState(prevState => {
+          return {
+            attachedClasses : [classes.SideBar, classes.Open]
+          }
+        });
+      } else {
+        this.setState(prevState => {
+          return {
+            attachedClasses: [classes.SideBar, classes.Close]
+          }
+        });
       }
-    // }
+    }
   }
 
   render() {
     return (
       <Fragment>
-        {this.props.showSidebar ? <Backdrop closeSideBar={this.props.closeSideBar} /> : null }
-        <div className={this.attachedClasses.join(" ")} >
+        {this.props.showSidebar ? (
+          <Backdrop closeSideBar={this.props.closeSideBar} />
+        ) : null}
+        <div className={this.state.attachedClasses.join(" ")}>
           <div>
-            <FaBars className={classes.HamburgerIcon} onClick={this.props.toggleSideBar} />
+            <FaBars
+              className={classes.HamburgerIcon}
+              onClick={this.props.toggleSideBar}
+            />
           </div>
           <h2>Sidebar</h2>
         </div>
